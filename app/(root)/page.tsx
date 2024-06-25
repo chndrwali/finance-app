91fbeeb30dfdbbea46013f0706c5c03afeedf3f1
chndrwali <chndrwali@gmail.com>
2024-06-25 17:31:06 +0700

    add chart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,19 @@
+import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+
 const Home = () => {
-  return <>Home</>;
+  const loggedIn = { firstName: 'Candra' };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox type="greeting" title="Welcome" user={loggedIn?.firstName || 'Guest'} subtext="Access and manage your account and transaction efficiently" />
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
