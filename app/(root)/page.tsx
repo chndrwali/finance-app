48b20c410cd7fbc344811381c05edd2371dccf46
chndrwali <chndrwali@gmail.com>
2024-06-26 23:52:04 +0700

    add transactions components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,37 @@
 import HeaderBox from '@/components/HeaderBox';
+import RecentTransactions from '@/components/RecentTransactions';
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-const Home = async () => {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({
+    userId: loggedIn.$id,
+  });
+
+  if (!accounts) return;
+
+  const accountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+  const account = await getAccount({ appwriteItemId });
 
   return (
     <section className="home">
       <div className="home-content">
         <header className="home-header">
-          <HeaderBox type="greeting" title="Welcome" user={loggedIn?.firstName || 'Guest'} subtext="Access and manage your account and transaction efficiently" />
-          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
+          <HeaderBox type="greeting" title="Selamat Datang" user={loggedIn?.firstName || 'Guest'} subtext="Kelola dan akses akun serta transaksi Anda secara efisien." />
+
+          <TotalBalanceBox accounts={accountsData} totalBanks={accounts?.totalBanks} totalCurrentBalance={accounts?.totalCurrentBalance} />
         </header>
-        Recent Transaction
+
+        <RecentTransactions accounts={accountsData} transactions={account?.transactions} appwriteItemId={appwriteItemId} page={currentPage} />
       </div>
-      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
+
+      <RightSidebar user={loggedIn} transactions={account?.transactions} banks={accountsData?.slice(0, 2)} />
     </section>
   );
 };
